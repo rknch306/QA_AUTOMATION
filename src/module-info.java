ec57a6e81543f3b05fd22be6a4522ec362947013
rknch306 <rknch306@gmail.com>
2023-12-14 15:42:25 +0530

    1st commit

---
+++ src/module-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module AUTOMATION_SELENIUM {
+}
---
+++ src/package1/Browser_Launch.java
@@ -0,0 +1,11 @@
+package package1;
+
+public class Browser_Launch {
+
+	public static void main(String[] args) {
+		System.setProperty("webdriver.edge.driver","D:\\msedgedriver.exe");
+		
+
+	}
+
+}
